397ca6f93567443e2f2fe67d6afb4affd937c690
xuanwang <xuanwang@ncs.com.sg>
2019-01-17 16:02:38 +0800

    updated ng

--- Ng/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Ng/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
+import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
@@ -37,4 +37,13 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => this.heroes.push(hero));
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
